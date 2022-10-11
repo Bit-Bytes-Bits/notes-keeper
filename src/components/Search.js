3448cbe21ab12db065b17124b02f197bd624a053
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-11 21:39:32 +0300

    define callback function for searching through notes and receive prop from App

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function Search() {
-  function handleSearch(){
+function Search({setSearch}) {
+  function handleSearch(e){
     // console.log(e.target.value)
+    setSearch(e.target.value)
   }
 
   return (
