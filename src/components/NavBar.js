9ef019decea22d60bea68add939bffc3aff40d4e
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-12 21:18:23 +0300

    add className to NavLink and span to delete button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 function NavBar() {
   return (
     <div className='nav'>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/addnotes">Add Notes</NavLink>
+      <NavLink className="navlink" to="/">Home</NavLink>
+      <NavLink className="navlink" to="/addnotes">Add Notes</NavLink>
       {/* <NavLink to="/notes">View Notes</NavLink> */}
       
     </div>
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function NotesList({id, note,onDelete}) {
   return (
-    <div>
-      <h3>category:{note.category}</h3>
-      <h4>Date:{note.date}</h4>
-      <p>Note:{note.text}</p>
-      <button onClick={()=>onDelete(id)}>DELETE</button>
+    <div className="noteList">
+      <h4>Category: {note.category}</h4>
+      <h4>Date: {note.date}</h4>
+      <p>Note:
+      <br></br>{note.text}</p>
+      <button className="deleteBtn" onClick={()=>onDelete(id)}><span class="material-symbols-rounded">X
+</span></button>
     </div>
   );
 }
